2c5de9fa4969b6f3345b762b52fc56f72c5cf8b8
Majed-intelli <83696933+Majed-intelli@users.noreply.github.com>
2021-10-17 10:16:37 +0200

    Update test2.py
    remove mm

--- test2.py
+++ test2.py
@@ -8,7 +8,6 @@ search_bar = driver.find_element_by_name("q")
 search_bar.clear()
 search_bar.send_keys("getting started with python")
 search_bar.send_keys(Keys.RETURN)
-mm
 print('The current url is: ')
 
 print(driver.current_url)
