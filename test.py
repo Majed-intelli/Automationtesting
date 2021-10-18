30eb29470c76ee32624b263df15fae23e1290ed5
altaher <altaher.gafar@iatl-intellisoft.com>
2021-10-18 10:27:17 +0200

    I'm add time

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Firefox
+import time
 
 driver = Firefox()
 Firefox()
